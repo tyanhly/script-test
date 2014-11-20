73a38f4fb62613767c535039a56731a87b253c9d
Tung Ly <tyanhly@yahoo.com>
2014-11-20 12:19:46 +0700

    ignore

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ echo "
 ##############################"
 echo "#Acquire::http::proxy \"http://$APT_PROXY_IP:3142\";" > /etc/apt/apt.conf;
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+git config --global user.name "tyanhly"
+git config --global user.email tyanhly@example.com
+git config --global push.default simple
 
 echo "ifconfig $PUB_INTERFACE  $PUB_IP netmask $PUB_NETMASK"
 ifconfig $PUB_INTERFACE $PUB_IP netmask $PUB_NETMASK
