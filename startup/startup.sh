3900ad98ea0956d853e3790eed5048bbf6af3223
root <tyanhly@yahoo.com>
2014-11-22 02:16:50 +0700

    all

--- startup/startup.sh
+++ startup/startup.sh
@@ -4,6 +4,10 @@ echo $dir
 source $dir/scan-net.sh
 $dir/network.php
 
+ulimit -n 10000000
+cat kisssysctl.conf > /etc/sysctl.conf
+sysctl -p
+
 git config --global user.name "tyanhly"
 git config --global user.email tyanhly@yahoo.com
 git config --global push.default simple
