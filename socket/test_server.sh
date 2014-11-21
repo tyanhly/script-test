a3c57bc3eb23e88ffd9d39c86735ec6e48203db9
tyanhly <tyanhly@example.com>
2014-11-21 22:24:41 +0700

    all

--- socket/test_server.sh
+++ socket/test_server.sh
@@ -2,4 +2,4 @@
 source config.sh
 ulimit -n $ULIMIT
 echo "python server_connection.py $SERVER_IP_FIRST $SERVER_IP_END $SERVER_IP_PREFIX $SERVER_PORT"
-python server_connection.py $SERVER_IP_FIRST $SERVER_IP_END $SERVER_IP_PREFIX $SERVER_PORT
+python server_connection.py $SERVER_IP_MID_FIRST $SERVER_IP_MID_END $SERVER_IP_PREFIX $SERVER_PORT
