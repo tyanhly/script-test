d850ea7509e93cc0473ee15ed4a2a35688506acf
tyanhly <tyanhly@example.com>
2014-11-21 13:48:24 +0700

    all

--- nfs.sh
+++ nfs.sh
@@ -13,7 +13,7 @@ apt-get -y install nfs-kernel-server
 mkdir /nfsroot/live -p
 cp $PUB_NFS_OS_FILE /nfsroot/live
 cat <<EOF >/etc/exports
-/nfsroot  $PUB_SUBNET/$PUB_NFS_BITMASK(fsid=0,ro,no_root_squash,insecure,no_subtree_check)
+/nfsroot  $PUB_SUBNET/$PUB_NFS_BITMASK(fsid=0,rw,no_root_squash,async,insecure,no_subtree_check)
 EOF
 
 exportfs -a
