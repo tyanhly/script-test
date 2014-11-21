f71baae2487a678ef6891a528edb13edb4adf053
tyanhly <tyanhly@yahoo.com>
2014-11-22 02:40:59 +0700

    startup edit

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-dir=$(dirname $SOURCE_BASH[0])
 
+if [ ! -f /tmp/kiss_startup_check ]; then
+dir=$(dirname $0)
+echo $dir
 source $dir/scan-net.sh
 $dir/network.php
 
 git config --global user.name "tyanhly"
 git config --global user.email tyanhly@yahoo.com
 git config --global push.default simple
+touch /tmp/kiss_startup_check
+
+fi
