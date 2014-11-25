4824d0ebcf7d397ac1c522c6e27b256e67de96dd
Tung Ly <tyanhly@yahoo.com>
2014-11-25 11:01:55 +0700

    all

--- socket/client_msg.py
+++ socket/client_msg.py
@@ -14,7 +14,7 @@ msg_amount = 1
 cur_msg_amount = 0
 pre_msg_amount = 0
 
-
+process_amount = 0;
 
 
 ##################################################
@@ -35,15 +35,17 @@ print "Your Msg: '" + msg + "', Amount: " + str(msg_amount)
 
 
 def handle(server_ip, server_port, msg):
-  
+  global process_amount
+  process_amount += 1
   sock = socket.socket()
   print "Start send msg: " + msg + " to " + server_ip + ":" + str(server_port)
   sock.connect((str(server_ip), int(server_port)))
   sock.sendall(msg)
   data = sock.recv(1024)
   if len(data) >0 :
-    print 'Received from ' + server_ip + ":" + repr(data)
+    print str(process_amount) + ' - Received from ' + server_ip + ":" + repr(data)
     sock.close()
+    process_amount -=1
   
 
 def server():
@@ -53,16 +55,19 @@ def server():
   global server_ip
   global server_port
   global msg
+
   p_time = time()
   while(cur_msg_amount <  msg_amount):
     cur_msg_amount += 1
     Greenlet.spawn(handle, server_ip, server_port, msg)
     sleep(0)
-  sleep(100)
+ 
 
 ##################################################
 
 
 g = Greenlet.spawn(server);
 g.join()
-
+while(process_amount > 0):
+  print "Process amount: " + str(process_amount)
+  sleep(1)
