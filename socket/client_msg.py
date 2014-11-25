0258a39124de7f027efa6487c7b5ef3c02b2bbf9
Tung Ly <tyanhly@yahoo.com>
2014-11-25 10:15:06 +0700

    all

---
+++ socket/client_msg.py
@@ -0,0 +1,68 @@
+import gevent
+import sys
+import string
+
+from time import time
+from gevent import socket
+from gevent import sleep
+from gevent import Greenlet
+##################################################
+print "===============CLIENT CONNECTION====================="
+##################################################
+
+msg_amount = 1
+cur_msg_amount = 0
+pre_msg_amount = 0
+
+
+
+
+##################################################
+if len(sys.argv) < 4:
+  print "params: <server_ip> <server_port> <msg> [msg_amount]"
+  sys.exit()
+
+server_ip = sys.argv[1]
+server_port = int(sys.argv[2])
+msg = sys.argv[3]
+
+if len(sys.argv) > 4:
+  msg_amount = int(sys.argv[4])
+
+print "Your Msg: '" + msg + "', Amount: " + str(msg_amount)
+
+##################################################
+
+
+def handle(server_ip, server_port, msg):
+  
+  sock = socket.socket()
+  print "Start send msg: " + msg + " to " + server_ip + ":" + str(server_port)
+  sock.connect((str(server_ip), int(server_port)))
+  sock.sendall(msg)
+  data = sock.recv(1024)
+  if len(data) >0 :
+    print 'Received from ' + server_ip + ":" + repr(data)
+    sock.close()
+  
+
+def server():
+  global msg_amount
+  global cur_msg_amount
+  global pre_msg_amount
+  global server_ip
+  global server_port
+  global msg
+  p_time = time()
+  while(cur_msg_amount <  msg_amount):
+    cur_msg_amount += 1
+    Greenlet.spawn(handle, server_ip, server_port, msg)
+    sleep(0)
+  sleep(100)
+
+##################################################
+
+
+g = Greenlet.spawn(server);
+g.join()
+
---
+++ socket/server_msg.py
@@ -0,0 +1,68 @@
+import gevent
+import sys
+from time import time
+from gevent import socket
+from gevent import sleep
+from gevent import Greenlet
+
+
+##################################################
+print "===============SERVER CONNECTION====================="
+##################################################
+
+server_port = 8000
+connection_amount = 0
+p_time = time()
+connection_amount_pool = []
+rate = []
+
+server_ip_pool = []
+##################################################
+if len(sys.argv) < 2:
+  print "params: <server_server_ip> [server_port]"
+  sys.exit()
+
+server_server_ip = sys.argv[1]
+server_port = int(sys.argv[2])
+
+##################################################
+
+def echo(sock, address):
+    global connection_amount
+    global p_time
+    global rate
+    connection_amount+=1
+    t = time()
+    rate.append(t)
+
+    if t - p_time > 1:
+      print("connection_amount %s, %s" % (connection_amount, len(rate)))
+      p_time = t
+
+    t = t - 1
+    while(t > rate[0]):
+      rate.pop(0)
+
+    m = sock.recv(1024);
+    sock.sendall(m);
+    print "Client:" + str(address) + ", Send msg: " + m;
+    connection_amount_pool.append(sock)
+
+ 
+def server(server_ip, server_port) :
+  global client_ip_pool
+  sock = socket.socket()
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  print server_ip + " " + str(server_port)
+  sock.bind((server_ip, server_port))
+  sock.listen(65000)
+  while(1):
+    csock, address = sock.accept()
+    
+    Greenlet.spawn(echo, csock, address)
+    sleep(0)
+
+
+g = Greenlet.spawn(server, server_server_ip, server_port)
+
+g.join()
